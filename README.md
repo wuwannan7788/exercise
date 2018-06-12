7ad9023340edd505e23188c8b6d940077e9ce61c
wuwannan7788 <1019397809@qq.com>
2018-06-12 17:26:48 +0800

    update catalog

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 
 # exercise book
-Hello World!
+练习册 exercise-book
+
+## 简介
+作者很懒
+
+## 安装下载
+
+## 快速使用
+
+## 交流
+
+## 关于作者
+- 个人主页 ：巴山夜话
+
+
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Administrator on 2018/6/12.
+ */
+module.exports = {
+  mode:'development',
+  entry: './src/index.js',
+  output: {
+    path: __dirname,
+    filename: './release/bundle.js'
+  },
+  module:{
+    rules:[
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+}
